b3ea1c3637b310031d4f564e588c149d2ff85a99
DILSHAN565 <65005789+DILSHAN565@users.noreply.github.com>
2023-11-03 11:27:11 +0530

    njde

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World lukluk changes This is your one ");
         System.out.println("Ehemada Asa oyala ");
+        System.out.println("Just do it");
 
     }
 }
